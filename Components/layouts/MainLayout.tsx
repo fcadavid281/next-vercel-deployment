9bb2916c88be74e58c0f06e3258ce005b0a1a480
Fabian Cadavid <fabiancadavid281@gmail.com.co>
2023-03-03 09:57:55 -0500

    Modificacion del Layout

--- Components/layouts/MainLayout.tsx
+++ Components/layouts/MainLayout.tsx
@@ -2,13 +2,17 @@
 import styles from "../../Components/layouts/MainLayout.module.css";
 import Head from "next/head";
 import { Navbar } from "../Navbar";
-import { FC } from "react";
+import { FC, PropsWithChildren } from "react";
 
-export const MainLayout: FC = ({ children }: any) => {
+interface LayoutProps extends PropsWithChildren<{}> {
+  title?: string;
+}
+
+export const MainLayout: FC<LayoutProps> = ({ children, title }) => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Fabian Cadavid</title>
+        <title>{title || "Fabian Cadavid"}</title>
         <meta name="description" content="Home App" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
